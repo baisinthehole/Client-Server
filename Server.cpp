c8f1458f9a698c9ae559c1197f68b79b8814c0ae
baisinthehole <helmer.n@hotmail.com>
2016-09-12 00:21:54 +0200

    managed communication between client and server, but losing contact immediately after

--- Server.cpp
+++ Server.cpp
@@ -9,6 +9,10 @@
 #include <stdio.h>
 #include <iostream>
 #include <boost/thread.hpp>
+#include <boost/lexical_cast.hpp>
+#include <atomic>
+#include <string>
+
 
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
@@ -18,37 +22,110 @@
 #define DEFAULT_PORT "7778"
 #define MAX_CLIENTS 2
 
-void communicate(SOCKET &ListenSocket, int &iResult, int &iSendResult, char *recvbuf, int recvbuflen, bool &error) {
+boost::mutex lock;
 
-	SOCKET ClientSocket = INVALID_SOCKET;
+// struct that keeps info useful info about clients and threads
 
-	ClientSocket = accept(ListenSocket, NULL, NULL);
-	std::cout << "connected!" << std::endl;
-	if (ClientSocket == INVALID_SOCKET) {
+struct ClientInfo {
+	SOCKET ClientSockets[MAX_CLIENTS];
+
+	int threadIDs[MAX_CLIENTS];
+
+	std::atomic<int> activeThreadID{ 0 };
+
+	std::atomic<int> activeThreadIndex{ -1 };
+
+	bool errors[MAX_CLIENTS];
+
+	std::atomic<int> receiveSignal{ 0 };
+
+	std::atomic<int> numBytes{ 0 };
+};
+
+// This function is returning a thread ID as an unsigned long. Call it inside a thread to get the thread ID
+unsigned long getThreadId() {
+	std::string threadId = boost::lexical_cast<std::string>(boost::this_thread::get_id());
+
+	unsigned long threadNumber = 0;
+
+	sscanf_s(threadId.c_str(), "%lx", &threadNumber);
+
+	return threadNumber;
+}
+
+// Converts a thread ID to index
+int convertFromThreadIdToIndex(int id, int threadIds[], int numClients) {
+	for (int i = 0; i < numClients; i++) {
+		if (threadIds[i] == id) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+void sendToClient(ClientInfo &clientInfo, int &iResult, int &iSendResult, int index, char* sendbuf) {
+	while (true) {
+		if (clientInfo.receiveSignal == 1 && clientInfo.activeThreadIndex != index) {
+			lock.lock();
+
+			iResult = clientInfo.numBytes;
+
+			iSendResult = send(clientInfo.ClientSockets[index], sendbuf, iResult, 0);
+			if (iSendResult == SOCKET_ERROR) {
+				printf("send failed with error: %d\n", WSAGetLastError());
+				closesocket(clientInfo.ClientSockets[index]);
+				WSACleanup();
+				clientInfo.errors[index] = 1;
+			}
+			std::cout << "Bytes sent to " << clientInfo.threadIDs[index] << " = " << iSendResult << std::endl;
+
+			clientInfo.receiveSignal = 0;
+			clientInfo.activeThreadIndex = -1;
+			lock.unlock();
+		}
+	}
+}
+
+void communicate(SOCKET &ListenSocket, ClientInfo &clientInfo, int &iResult, int &iSendResult) {
+
+	int index = convertFromThreadIdToIndex(clientInfo.activeThreadID, clientInfo.threadIDs, MAX_CLIENTS);
+
+	clientInfo.ClientSockets[index] = accept(ListenSocket, NULL, NULL);
+
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	clientInfo.threadIDs[index] = getThreadId();
+	std::cout << clientInfo.threadIDs[index] << " connected!" << std::endl;
+	if (clientInfo.ClientSockets[index] == INVALID_SOCKET) {
 		printf("accept failed with error: %d\n", WSAGetLastError());
 		closesocket(ListenSocket);
 		WSACleanup();
-		error = 1;
+		clientInfo.errors[index] = 1;
 	}
+
+	boost::thread sender(sendToClient, std::ref(clientInfo), iResult, iSendResult, index, recvbuf);
 
 	// Receive until the peer shuts down the connection
 	while (true) {
-		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+		iResult = recv(clientInfo.ClientSockets[index], recvbuf, recvbuflen, 0);
+		lock.lock();
+		clientInfo.activeThreadID = getThreadId();
+
+		clientInfo.activeThreadIndex = convertFromThreadIdToIndex(clientInfo.activeThreadID, clientInfo.threadIDs, MAX_CLIENTS);
+
+		clientInfo.receiveSignal = 1;
+
+		clientInfo.numBytes = iResult;
+
 		if (iResult > 0) {
+			std::cout << clientInfo.threadIDs[index] << " says: ";
 			for (int i = 0; i < iResult; i++) {
 				std::cout << recvbuf[i];
 			}
 			std::cout << std::endl;
-			// Send the buffer to all other clients
-
-			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
-			if (iSendResult == SOCKET_ERROR) {
-				printf("send failed with error: %d\n", WSAGetLastError());
-				closesocket(ClientSocket);
-				WSACleanup();
-				error = 1;
-			}
-			printf("Bytes sent: %d\n", iSendResult);
+			
+			std::cout << "Bytes received from " << clientInfo.threadIDs[index] << " = " << iResult << std::endl;
 
 		}
 		if (recvbuf[0] == '0') {
@@ -57,47 +134,13 @@ void communicate(SOCKET &ListenSocket, int &iResult, int &iSendResult, char *rec
 		}
 		if (iResult == SOCKET_ERROR) {
 			printf("recv failed with error: %d\n", WSAGetLastError());
-			closesocket(ClientSocket);
+			closesocket(clientInfo.ClientSockets[index]);
 			WSACleanup();
-			error = 1;
+			clientInfo.errors[index] = 1;
 		}
+		lock.unlock();
 	}
-	/*
-	// Receive until the peer shuts down the connection
-	while (true) {
-		iResult = recv(ClientSockets[socketIndex], recvbuf, recvbuflen, 0);
-		if (iResult > 0) {
-			for (int i = 0; i < iResult; i++) {
-				std::cout << recvbuf[i];
-			}
-			std::cout << std::endl;
-			// Send the buffer to all other clients
-			for (int i = 0; i < MAX_CLIENTS; i++) {
-				if (i != socketIndex) {
-					iSendResult = send(ClientSockets[i], recvbuf, iResult, 0);
-					if (iSendResult == SOCKET_ERROR) {
-						printf("send failed with error: %d\n", WSAGetLastError());
-						closesocket(ClientSockets[i]);
-						WSACleanup();
-						error = true;
-						return;
-					}
-					printf("Bytes sent: %d\n", iSendResult);
-				}
-			}
-		}
-		if (recvbuf[0] == '0') {
-			printf("Connection closing...\n");
-			break;
-		}
-		if (iResult == SOCKET_ERROR) {
-			printf("recv failed with error: %d\n", WSAGetLastError());
-			closesocket(ClientSockets[socketIndex]);
-			WSACleanup();
-			error = true;
-			return;
-		}
-	}*/
+	sender.join();
 }
 
 int __cdecl main(void)
@@ -106,15 +149,12 @@ int __cdecl main(void)
 	WSADATA wsaData;
 	int iResult;
 
-	SOCKET ClientSocket = INVALID_SOCKET;
 	SOCKET ListenSocket = INVALID_SOCKET;
 
 	struct addrinfo *result = NULL;
 	struct addrinfo hints;
 
 	int iSendResult = 0;
-	char recvbuf[DEFAULT_BUFLEN];
-	int recvbuflen = DEFAULT_BUFLEN;
 
 	// Initialize Winsock
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -166,21 +206,29 @@ int __cdecl main(void)
 		return 1;
 	}
 
-	// Receive until the peer shuts down the connection
-
-	bool errors[MAX_CLIENTS] = { false, false };
-	
-	boost::thread firstClient(communicate, ListenSocket, iResult, iSendResult, recvbuf, recvbuflen, errors[0]);
-	boost::thread secondClient(communicate, ListenSocket, iResult, iSendResult, recvbuf, recvbuflen, errors[1]);
+	ClientInfo clientInfo;
 
 	for (int i = 0; i < MAX_CLIENTS; i++) {
-		if (errors[i] == 1) {
+		clientInfo.errors[i] = false;
+		clientInfo.ClientSockets[i] = INVALID_SOCKET;
+		clientInfo.threadIDs[i] = 0;
+	}
+
+	boost::thread firstClient(communicate, ListenSocket, std::ref(clientInfo), iResult, iSendResult);
+	boost::thread secondClient(communicate, ListenSocket, std::ref(clientInfo), iResult, iSendResult);
+
+	while (true) {
+		
+	}
+
+	for (int i = 0; i < MAX_CLIENTS; i++) {
+		if (clientInfo.errors[i] == 1) {
 			return 1;
 		}
 	}
 
 	firstClient.join();
-	//secondClient.join();
+	secondClient.join();
 
 	system("pause");
 	return 0;
